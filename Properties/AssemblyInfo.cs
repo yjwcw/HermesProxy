2f3984e2fadd0498405dbc1117d6ec7425d01534
yjwcw <32291264+yjwcw@users.noreply.github.com>
2026-07-03 15:57:01 +0800

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyFileVersion("3.10.0.0")]
+[assembly: AssemblyInformationalVersion("3.10.0+Branch.master.Sha.841a26f5c06679680fd5a396922c293d36e84b71")]
+[assembly: AssemblyCompany("WowLegacyCore")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCopyright("Copyright © WowLegacyCore 2023")]
+[assembly: AssemblyProduct("HermesProxy")]
+[assembly: AssemblyTitle("HermesProxy")]
+[assembly: AssemblyVersion("3.10.0.0")]
