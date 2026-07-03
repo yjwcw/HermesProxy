b889910afc4804a958a6b17f67cab64edc5e04c8
yjwcw <32291264+yjwcw@users.noreply.github.com>
2026-07-03 15:21:04 +0800

    Add files via upload

---
+++ HermesProxy.World.Enums.V3_3_5_12340/ContainerField.cs
@@ -0,0 +1,9 @@
+namespace HermesProxy.World.Enums.V3_3_5_12340;
+
+public enum ContainerField
+{
+	CONTAINER_FIELD_NUM_SLOTS = 64,
+	CONTAINER_ALIGN_PAD = 65,
+	CONTAINER_FIELD_SLOT_1 = 66,
+	CONTAINER_END = 138
+}
---
+++ HermesProxy.World.Enums.V3_3_5_12340/ObjectField.cs
@@ -0,0 +1,11 @@
+namespace HermesProxy.World.Enums.V3_3_5_12340;
+
+public enum ObjectField
+{
+	OBJECT_FIELD_GUID = 0,
+	OBJECT_FIELD_TYPE = 2,
+	OBJECT_FIELD_ENTRY = 3,
+	OBJECT_FIELD_SCALE_X = 4,
+	OBJECT_FIELD_PADDING = 5,
+	OBJECT_END = 6
+}
