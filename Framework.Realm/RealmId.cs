8046b8e19812ecdd5132039fc747ec69d6dca91f
yjwcw <32291264+yjwcw@users.noreply.github.com>
2026-07-03 15:13:38 +0800

    Add files via upload

---
+++ Framework.Realm/RealmId.cs
@@ -0,0 +1,65 @@
+using System;
+
+namespace Framework.Realm;
+
+public struct RealmId : IEquatable<RealmId>
+{
+	public uint Index { get; set; }
+
+	public byte Region { get; set; }
+
+	public byte Site { get; set; }
+
+	public RealmId(byte region, byte battlegroup, uint index)
+	{
+		Region = region;
+		Site = battlegroup;
+		Index = index;
+	}
+
+	public RealmId(uint realmAddress)
+	{
+		Region = (byte)((realmAddress >> 24) & 0xFFu);
+		Site = (byte)((realmAddress >> 16) & 0xFFu);
+		Index = realmAddress & 0xFFFFu;
+	}
+
+	public uint GetAddress()
+	{
+		return (uint)((Region << 24) | (Site << 16) | (ushort)Index);
+	}
+
+	public string GetAddressString()
+	{
+		return $"{Region}-{Site}-{Index}";
+	}
+
+	public string GetSubRegionAddress()
+	{
+		return $"{Region}-{Site}-0";
+	}
+
+	public override bool Equals(object obj)
+	{
+		if (obj != null && obj is RealmId)
+		{
+			return Equals((RealmId)obj);
+		}
+		return false;
+	}
+
+	public bool Equals(RealmId other)
+	{
+		return other.Index == Index;
+	}
+
+	public override int GetHashCode()
+	{
+		return new { Site, Region, Index }.GetHashCode();
+	}
+
+	public override string ToString()
+	{
+		return $"Realm{{Index={Index},Region={Region},Index={Index}}}";
+	}
+}
