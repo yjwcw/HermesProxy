7f1d3cd91a641d33773ede1611e60b9c3b599bdf
yjwcw <32291264+yjwcw@users.noreply.github.com>
2026-07-03 15:20:34 +0800

    Add files via upload

---
+++ HermesProxy.World.Enums.V2_5_3_41750/ConversationDynamicField.cs
@@ -0,0 +1,8 @@
+namespace HermesProxy.World.Enums.V2_5_3_41750;
+
+public enum ConversationDynamicField
+{
+	CONVERSATION_DYNAMIC_FIELD_ACTORS,
+	CONVERSATION_DYNAMIC_FIELD_LINES,
+	CONVERSATION_DYNAMIC_END
+}
---
+++ HermesProxy.World.Enums.V2_5_3_41750/PlayerDynamicField.cs
@@ -0,0 +1,7 @@
+namespace HermesProxy.World.Enums.V2_5_3_41750;
+
+public enum PlayerDynamicField
+{
+	PLAYER_DYNAMIC_FIELD_ARENA_COOLDOWNS = 3,
+	PLAYER_DYNAMIC_END
+}
