190523d0ca4672e42f9ee89086734e631dfb6ef6
yjwcw <32291264+yjwcw@users.noreply.github.com>
2026-07-03 15:21:54 +0800

    Add files via upload

---
+++ HermesProxy.World.Enums.WotLK/EnchantmentSlot.cs
@@ -0,0 +1,32 @@
+namespace HermesProxy.World.Enums.WotLK;
+
+public static class EnchantmentSlot
+{
+	public static int Perm = 0;
+
+	public static int Temp = 1;
+
+	public static int Sock1 = 2;
+
+	public static int Sock2 = 3;
+
+	public static int Sock3 = 4;
+
+	public static int Bonus = 5;
+
+	public static int Prismatic = 6;
+
+	public static int MaxInspected = 7;
+
+	public static int Prop0 = 7;
+
+	public static int Prop1 = 8;
+
+	public static int Prop2 = 9;
+
+	public static int Prop3 = 10;
+
+	public static int Prop4 = 11;
+
+	public static int Max = 12;
+}
