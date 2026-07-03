a878d337baecae2bf4fb3bd9d4e6511506c82538
yjwcw <32291264+yjwcw@users.noreply.github.com>
2026-07-03 15:32:04 +0800

    Add files via upload

---
+++ HermesProxy.World.Objects.Version.V1_14_0_40237/AreaTriggerDynamicField.cs
@@ -0,0 +1,6 @@
+namespace HermesProxy.World.Enums.V1_14_0_40237;
+
+public enum AreaTriggerDynamicField
+{
+	AREATRIGGER_DYNAMIC_END
+}
---
+++ HermesProxy.World.Objects.Version.V1_14_0_40237/PlayerDynamicField.cs
@@ -0,0 +1,7 @@
+namespace HermesProxy.World.Enums.V1_14_0_40237;
+
+public enum PlayerDynamicField
+{
+	PLAYER_DYNAMIC_FIELD_ARENA_COOLDOWNS = 3,
+	PLAYER_DYNAMIC_END
+}
